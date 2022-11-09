foundriesio/optee_os

commit ba655f6351baa28a904e217c599f21c7bef80e3e

[FIO toup] Support overlay at fix address and extend DTS
The following commit adds support to extend any DTS passed as an input
parameter to OPTEE during boot as well as generate a OPTEE specific
DTS overlay at a build specific address (CFG_OVERLAY_ADDR).

When this feature is enabled, CFG_DT_ADDR and CFG_EXTERNAL_DTB_OVERLAY
are no longer required.

Signed-off-by: Jorge Ramirez-Ortiz <jorge@foundries.io>
Signed-off-by: Ricardo Salveti <ricardo@foundries.io>

core/arch/arm/kernel/boot.c
@@ -79,6 +79,9 @@ struct dt_descriptor {
 	void *blob;
 #ifdef _CFG_USE_DTB_OVERLAY
 	int frag_id;
+#ifdef CFG_OVERLAY_ADDR
+	int is_overlay;
+#endif
 #endif
 };
 
@@ -661,7 +664,9 @@ static TEE_Result release_external_dt(void)
 
 	return TEE_SUCCESS;
 }
+#ifndef CFG_OVERLAY_ADDR
 boot_final(release_external_dt);
+#endif
 
 #ifdef _CFG_USE_DTB_OVERLAY
 static int add_dt_overlay_fragment(struct dt_descriptor *dt, int ioffs)
@@ -670,6 +675,10 @@ static int add_dt_overlay_fragment(struct dt_descriptor *dt, int ioffs)
 	int offs;
 	int ret;
 
+#ifdef CFG_OVERLAY_ADDR
+	if (!dt->is_overlay)
+		return ioffs;
+#endif
 	snprintf(frag, sizeof(frag), "fragment@%d", dt->frag_id);
 	offs = fdt_add_subnode(dt->blob, ioffs, frag);
 	if (offs < 0)
@@ -1118,6 +1127,9 @@ static void init_external_dt(unsigned long phys_dt)
 
 	dt->blob = fdt;
 
+#ifdef CFG_OVERLAY_ADDR
+	if (dt->is_overlay) {
+#endif
 	ret = init_dt_overlay(dt, CFG_DTB_MAX_SIZE);
 	if (ret < 0) {
 		EMSG("Device Tree Overlay init fail @ %#lx: error %d", phys_dt,
@@ -1125,6 +1137,9 @@ static void init_external_dt(unsigned long phys_dt)
 		panic();
 	}
 
+#ifdef CFG_OVERLAY_ADDR
+	}
+#endif
 	ret = fdt_open_into(fdt, fdt, CFG_DTB_MAX_SIZE);
 	if (ret < 0) {
 		EMSG("Invalid Device Tree at %#lx: error %d", phys_dt, ret);
@@ -1411,6 +1426,10 @@ static void init_primary(unsigned long pageable_part, unsigned long nsec_entry)
  */
 void __weak boot_init_primary_late(unsigned long fdt)
 {
+#ifdef CFG_OVERLAY_ADDR
+	struct dt_descriptor *dt = &external_dt;
+	dt->is_overlay = 0;
+#endif
 	init_external_dt(fdt);
 	tpm_map_log_area(get_external_dt());
 	discover_nsec_memory();
@@ -1439,6 +1458,15 @@ void __weak boot_init_primary_late(unsigned long fdt)
 	} else {
 		init_tee_runtime();
 	}
+
+#ifdef CFG_OVERLAY_ADDR
+	release_external_dt();
+	dt->is_overlay = 1;
+	init_external_dt(CFG_OVERLAY_ADDR);
+	update_external_dt();
+	release_external_dt();
+#endif
+
 	call_finalcalls();
 	IMSG("Primary CPU switching to normal world boot");
 }

mk/config.mk
@@ -450,6 +450,10 @@ CFG_DTB_MAX_SIZE ?= 0x10000
 CFG_SP_INIT_INFO_MAX_SIZE ?= 0x1000
 
 # Device Tree Overlay support.
+#
+# CASE 1
+# ------
+#
 # CFG_EXTERNAL_DTB_OVERLAY allows to append a DTB overlay into an existing
 # external DTB. The overlay is created when no valid DTB overlay is found.
 # CFG_GENERATE_DTB_OVERLAY allows to create a DTB overlay at external
@@ -461,11 +465,33 @@ CFG_SP_INIT_INFO_MAX_SIZE ?= 0x1000
 CFG_EXTERNAL_DTB_OVERLAY ?= n
 CFG_GENERATE_DTB_OVERLAY ?= n
 
+ifeq ($(CFG_OVERLAY_ADDR),)
 ifeq (y-y,$(CFG_EXTERNAL_DTB_OVERLAY)-$(CFG_GENERATE_DTB_OVERLAY))
 $(error CFG_EXTERNAL_DTB_OVERLAY and CFG_GENERATE_DTB_OVERLAY are exclusive)
 endif
 _CFG_USE_DTB_OVERLAY := $(call cfg-one-enabled,CFG_EXTERNAL_DTB_OVERLAY \
 			  CFG_GENERATE_DTB_OVERLAY)
+endif
+#
+# CASE 2
+# ------
+# This define enables support for an OP-TEE provided DTB overlay as well as
+# extending a device tree that must be passed as an input parameter.
+# This define is not compatible with CFG_EXTERNAL_DTB_OVERLAY nor
+# CFG_GENERATE_DTB_OVERLAY nor CFG_DT_ADDR and a build error should trigger
+# if either of those are enabled
+ifneq ($(strip $(CFG_OVERLAY_ADDR)),)
+ifeq ($(CFG_EXTERNAL_DTB_OVERLAY),y)
+$(error Cannot implement OVERLAY_ADDR and EXTERNAL_DTB_OVERLAY)
+endif
+ifeq ($(CFG_GENERATE_DTB_OVERLAY),y)
+$(error Cannot implement OVERLAY_ADDR and GENERATE_DTB_OVERLAY)
+endif
+ifneq ($(strip $(CFG_DT_ADDR)),)
+$(error Cannot implement OVERLAY_ADDR and CFG_DT_ADDR)
+endif
+$(call force,_CFG_USE_DTB_OVERLAY,y)
+endif
 
 # All embedded tests are supposed to be disabled by default, this flag
 # is used to control the default value of all other embedded tests

commit fcf11c1f03e8a249e745d2bfe437c863aedfba45

[FIO squash] core: arm: boot: set PSCI API for cpus in a DT overlay
Add an ability to switch cpus to PSCI method in a kernel DT with a
DT overlay.

How the method works:

When OP-TEE calls a function dt_add_psci_cpu_enable_methods() for a
u-boot DT, we mark each found ARM-A cpu as compatible to use over
overlay DT in a shared array.

When OP-TEE calls the function dt_add_psci_cpu_enable_methods() for
a DT kernel overlay, we add nodes from the shared array and set
"enable-method" to "psci" for them.

Fixes: ba655f6351ba ("[FIO toup] Support overlay at fix address and extend DTS")
Signed-off-by: Oleksandr Suvorov <oleksandr.suvorov@foundries.io>

core/arch/arm/kernel/boot.c
@@ -85,6 +85,10 @@ struct dt_descriptor {
 #endif
 };
 
+#ifdef CFG_OVERLAY_ADDR
+static bool cpu_psci_compat[CFG_TEE_CORE_NB_CORE];
+#endif
+
 static struct dt_descriptor external_dt __nex_bss;
 #endif
 
@@ -861,6 +865,8 @@ static int check_node_compat_prefix(struct dt_descriptor *dt, int offs,
 	return -1;
 }
 
+static int add_descriptor_node(struct dt_descriptor *dt, const char *driver);
+
 static int dt_add_psci_cpu_enable_methods(struct dt_descriptor *dt)
 {
 #define CPU_PATH_SIZE 20
@@ -874,14 +880,29 @@ static int dt_add_psci_cpu_enable_methods(struct dt_descriptor *dt)
 		if (ret < 0 || ret >= CPU_PATH_SIZE)
 			return -1;
 
-		noffset = fdt_path_offset(dt->blob, cpu_path);
-		if (noffset < 0)
-			continue;
+#ifdef CFG_OVERLAY_ADDR
+		if (!dt->is_overlay) {
+			cpu_psci_compat[cpu_idx] = false;
+#endif
+			noffset = fdt_path_offset(dt->blob, cpu_path);
+			if (noffset < 0)
+				continue;
 
-		if (check_node_compat_prefix(dt, noffset,
-					     "arm,cortex-a"))
-			continue; /* no compatible */
+			if (check_node_compat_prefix(dt, noffset,
+						     "arm,cortex-a"))
+				continue; /* no compatible */
+#ifdef CFG_OVERLAY_ADDR
+			else
+				cpu_psci_compat[cpu_idx] = true;
+		} else {
+			if (cpu_psci_compat[cpu_idx] == false)
+				continue;
 
+			noffset = add_descriptor_node(dt, cpu_path);
+			if (noffset < 0)
+				return -1;
+		}
+#endif
 		ret = fdt_setprop_string(dt->blob, noffset,
 					 "enable-method", "psci");
 		if (ret)
@@ -1478,6 +1499,11 @@ void __weak boot_init_primary_late(unsigned long fdt)
 	release_external_dt();
 	dt->is_overlay = 1;
 	init_external_dt(CFG_OVERLAY_ADDR);
+	/*
+	 * NOTE: call this function ONLY after calling it for u-boot
+	 * fdt. The 1st call with u-boot fdt marks psci-compatible
+	 * cpus in a shared array cpu_psci_compat[].
+	 */
 	update_external_dt();
 	release_external_dt();
 #endif
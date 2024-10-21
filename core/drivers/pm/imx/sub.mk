nxp-imx/imx-optee-os

pm: imx: add power management drivers
Add power management drivers for imx6, imx7 and imx8 platforms.

Signed-off-by: Clement Faure <clement.faure@nxp.com>

core/drivers/pm/imx/sub.mk
@@ -1,0 +1,14 @@
+global-incdirs-y += .
+
+subdirs-$(CFG_MX6)$(CFG_MX7) += busfreq
+subdirs-y += cpuidle
+subdirs-y += suspend
+
+srcs-$(CFG_PSCI_ARM32) += psci.c imx_ocram.c
+srcs-$(CFG_MX7) += pm-imx7.c gpcv2.c
+srcs-$(CFG_MX6) += pm-imx6.c
+srcs-$(CFG_MX6)$(CFG_MX7) +=  imx_src.c mmdc.c
+srcs-$(CFG_MX7ULP) += pm-imx7ulp.c
+srcs-$(CFG_MX8M)$(CFG_MX8ULP) += pm-imx8.c
+
+asm-defines-y += imx_pm_asm_defines.c

foundriesio/optee_os

core/drivers/pm/imx/sub.mk
@@ -1,3 +1,14 @@
-srcs-y += psci.c
-srcs-$(CFG_MX7) += gpcv2.c
-srcs-$(CFG_MX6)$(CFG_MX7) += src.c
+incdirs-y += .
+
+subdirs-$(CFG_MX6)$(CFG_MX7) += busfreq
+subdirs-y += cpuidle
+subdirs-y += suspend
+
+srcs-$(CFG_PSCI_ARM32) += psci.c imx_ocram.c
+srcs-$(CFG_MX7) += pm-imx7.c gpcv2.c
+srcs-$(CFG_MX6) += pm-imx6.c
+srcs-$(CFG_MX6)$(CFG_MX7) += src.c mmdc.c
+srcs-$(CFG_MX7ULP) += pm-imx7ulp.c
+srcs-$(CFG_MX8M)$(CFG_MX8ULP) += pm-imx8.c
+
+asm-defines-y += imx_pm_asm_defines.c